abed799e62be30131f7dedebc0c651367a7cc39a
Firouzfar <firouzfar.sebastien@gmail.com>
2021-09-27 18:53:59 +0200

    Mise en place une nouvelle page / put the new page

---
+++ admin/index.php
@@ -0,0 +1,68 @@
+<html>
+
+<head>
+    <title>Food order Website - Home page </title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <!-- Menu Section start-->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Admin</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Order</a></li>
+            </ul>
+        </div>
+    </div>
+    <!--Menu Section end-->
+
+    <!-- Main content Section start -->
+    <div class="main-content">
+        <div class="wrapper">
+            <strong>DASHBORAD</strong>
+            <div class="col-4 text-center">
+                <br/>
+                <h1>5 </h1>
+                categories
+            </div>
+
+            <div class="col-4 text-center">
+                <br/>
+                <h1>5 </h1>
+                categories
+            </div>
+
+            <div class="col-4 text-center">
+                <br/>
+                <h1>5 </h1>
+                categories
+            </div>
+
+            <div class="col-4 text-center">
+                <br/>
+                <h1>5 </h1>
+                categories
+            </div>
+
+            <div class="col-4 text-center">
+                <br/>
+                <h1>5 </h1>
+                categories
+            </div>
+        </div>
+    </div>
+    <!-- Main content Section end -->
+
+    <!-- Footer section starts -->
+    <div class="footer">
+        <div class="wrapper">
+            <p class="text-center">2021 All rights reserved, some restaurant. developed By <a href="">Sebastien firouzfar</p>
+        </div> 
+        <!-- Footer Section end -->
+
+</body>
+
+</html>
