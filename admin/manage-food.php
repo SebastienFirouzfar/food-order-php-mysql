e068ae2d29bb477b880e51ad2eee177acef114de
Firouzfar <firouzfar.sebastien@gmail.com>
2021-09-29 16:03:47 +0200

    Mise en place les contenues des pages et creation de manage foot et order

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -1,14 +1,10 @@
 <?php include('partials/menu.php'); ?>
 
-<!-- Main content Section start -->
 <div class="main-content">
     <div class="wrapper">
-        <h1>MANAGE ADMIN</h1>
-
-
-
+        <h1>MANAGE FOOD</h1>
     </div>
+
 </div>
-<!-- Main content Section end -->
 
 <?php include('partials/footer.php'); ?>
